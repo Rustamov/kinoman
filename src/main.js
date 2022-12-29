9897fafe5a0e0993b3abd52ba8c9bbe4f0fd7c10
Kemran Rustamov <rustamovkm@gmail.com>
2022-12-29 14:44:02 +0300

    step 3

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-import {createUserTitleTemplate} from './components/user-title';
-
-import {createMenuTemplate} from './components/menu';
-
 import {createFilmsTemplate} from './components/films';
 import {createFilmCardTemplate} from './components/film-card';
-
 import {createFilmDetailsTemplate} from './components/film-details';
-
-import {createShowMoreBtnTemplate} from './components/show-more-button';
-
 import {createFooterStatisticTemplate} from './components/footer-statistic';
+import {createMenuTemplate} from './components/menu';
+import {createShowMoreBtnTemplate} from './components/show-more-button';
+import {createUserTitleTemplate} from './components/user-title';
 
-const FILMS_COUNT = 5;
+import {generateFilms} from './mock/film';
+
+
+const FILMS_COUNT = 15;
+const SHOWING_FILMS_COUNT_ON_START = 5;
+const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
+
 const TOP_RATED_FILMS_COUNT = 2;
 const MOST_COMMENTED_FILMS_COUNT = 2;
 
@@ -27,26 +28,49 @@ const siteMainElement = document.querySelector(`.main`);
 render(siteMainElement, createMenuTemplate());
 render(siteMainElement, createFilmsTemplate());
 
+const films = generateFilms(FILMS_COUNT);
+
 const filmsListElement = siteMainElement.querySelector(`.films-list`);
 const filmsListContainerElement = siteMainElement.querySelector(`.films-list__container`);
-for (let i = 0; i < FILMS_COUNT; i++) {
-  render(filmsListContainerElement, createFilmCardTemplate());
-}
+
+let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+films.slice(0, showingFilmsCount).forEach((film) => {
+  render(filmsListContainerElement, createFilmCardTemplate(film));
+});
+
 render(filmsListElement, createShowMoreBtnTemplate());
+const showMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
+showMoreButton.addEventListener(`click`, () => {
+  const prevFilmsCount = showingFilmsCount;
+  showingFilmsCount += SHOWING_FILMS_COUNT_BY_BUTTON;
+
+  films.slice(prevFilmsCount, showingFilmsCount).forEach((film) => {
+    render(filmsListContainerElement, createFilmCardTemplate(film));
+  });
+
+  if (showingFilmsCount >= films.length) {
+    showMoreButton.remove();
+  }
+});
 
 
 const topRatedFilmsListContainerElement = siteMainElement.querySelectorAll(`.films-list--extra .films-list__container`)[0];
-for (let i = 0; i < TOP_RATED_FILMS_COUNT; i++) {
-  render(topRatedFilmsListContainerElement, createFilmCardTemplate());
-}
+
+const topRatedFilms = generateFilms(TOP_RATED_FILMS_COUNT);
+topRatedFilms.forEach((film) => {
+  render(topRatedFilmsListContainerElement, createFilmCardTemplate(film));
+});
 
 const mostCommentedFilmsListContainerElement = siteMainElement.querySelectorAll(`.films-list--extra .films-list__container`)[1];
-for (let i = 0; i < MOST_COMMENTED_FILMS_COUNT; i++) {
-  render(mostCommentedFilmsListContainerElement, createFilmCardTemplate());
-}
+
+const mostCommentedFilms = generateFilms(MOST_COMMENTED_FILMS_COUNT);
+mostCommentedFilms.forEach((film) => {
+  render(mostCommentedFilmsListContainerElement, createFilmCardTemplate(film));
+});
 
 
 const siteFooterElement = document.querySelector(`.footer`);
 render(siteFooterElement, createFooterStatisticTemplate());
 
+// render(siteFooterElement, createFilmDetailsTemplate(films[0]), `afterend`);
 
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+import {MONTH_NAMES} from "./const";
+
+const castTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
+export const formatReleaseDate = (date) => {
+  return `${date.getDate()} ${MONTH_NAMES[date.getMonth()]} ${date.getFullYear()}`;
+};
+
+export const formatCommentDate = (date) => {
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${castTimeFormat(date.getHours())}:${castTimeFormat(date.getMinutes())}`;
+};
